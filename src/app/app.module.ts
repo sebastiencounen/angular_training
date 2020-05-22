5945050d65920df73cca7f6554f74876aa19b6f8
Sébastien Counen <sebastien.counen@outlook.com>
2020-05-22 17:34:03 +0200

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { SinglePostComponent } from './post-list/single-post/single-post.component';
+import { PostFormComponent } from './post-list/post-form/post-form.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListItemComponent } from './post-list/post-list-item/post-list-item.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { PostsService } from './services/posts.service';
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    PostListComponent,
+    SinglePostComponent,
+    PostFormComponent,
+    HeaderComponent,
+    PostListItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService, AuthGuardService, PostsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

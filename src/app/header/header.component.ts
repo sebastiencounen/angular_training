cc693e5e884dc3eafcce389e712e9592ec598998
Sébastien Counen <sebastien.counen@outlook.com>
2020-05-22 19:22:24 +0200

    Inscription

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-header',
